502697e0255a2c7c3357f2a8c07d60b10584c771
kunyaoxu <29974104+kunyaoxu@users.noreply.github.com>
2018-06-02 12:43:39 +0800

    finish web crawler

--- zen_of_python.py
+++ zen_of_python.py
@@ -1,17 +1,19 @@
 # -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 from BaseHTTPServer import BaseHTTPRequestHandler
 import cgi
 import json
 import requests
 
+import urllib2
+from bs4 import BeautifulSoup
+import random
+
+import sys
+reload(sys)
+sys.setdefaultencoding('utf8')
+
 class TodoHandler(BaseHTTPRequestHandler):
-    """A simple TODO server
-
-    which can display and manage todos for you.
-    """
-
-    # Global instance to store todos. You should use a database in reality.
-
     def do_GET(self):
         # return all todos
 
@@ -21,11 +23,29 @@ class TodoHandler(BaseHTTPRequestHandler):
 
 
         self.send_response(200)
-        self.send_header('Content-type', 'text/html')
+        self.send_header('Content-type', 'text/html; charset=utf-8')
         self.end_headers()
-        res = requests.get('http://wiki.python.org.tw/The Zen Of Python')
-        print(res.text)
-        self.wfile.write("test")
+        page = requests.get('http://wiki.python.org.tw/The%20Zen%20Of%20Python').text
+        print(page.encode(encoding="utf-8", errors="strict"))
+#        quote_page = 'http://wiki.python.org.tw/The%20Zen%20Of%20Python'
+#        page = urllib2.urlopen(quote_page)
+        soup = BeautifulSoup(page, 'lxml')
+#        name_box = soup.find('p', 'line874')
+        name_box = soup.find_all('p', 'line874')
+
+        print("哈123")
+        print(soup.original_encoding)
+        print len(name_box)
+        index = random.randrange(0, len(name_box))
+        self.wfile.write(name_box[index].text.encode(encoding="utf-8", errors="strict"))
+#        for p in name_box:
+#            print p.get('class'), p.text
+#            self.wfile.write(p.text.encode(encoding="utf-8", errors="strict"))
+##        self.wfile.write(name_box)
+#        print name_box
+#        name_box = " ".join(x.text.encode('utf-8') for x in name_box)
+#        self.wfile.write(name_box)
+
 
 if __name__ == '__main__':
     # Start a simple server, and loop forever
